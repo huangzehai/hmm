93bd44734b9d653ade0b6be85a9653fae1e5ede5
huangzehai <chaogeek@foxmail.com>
2019-08-04 02:33:28 +0800

    添加length参数

--- hmm/trainingMultiObservationWithMultinomialHMM.py
+++ hmm/trainingMultiObservationWithMultinomialHMM.py
@@ -7,10 +7,12 @@ np.random.seed(42)
 millis = int(round(time.time()))
 model = hmm.MultinomialHMM(n_components=2, n_iter=1000, tol=0.0001, random_state=millis)
 X1 = [[0], [2], [4], [3], [3], [3]]
-X2 = [[1], [5], [6], [3], [3], [3]]
-X = np.concatenate([X1, X2])
-model.fit(X)
-test_set = [[0], [0], [4], [3], [3], [5]]
+X2 = [[1], [5], [6], [3], [3], [3], [3]]
+X3 = [[1], [2], [3], [3], [3], [3], [3]]
+lengths = [len(X1), len(X2), len(X3)]
+X = np.concatenate([X1, X2, X3])
+model.fit(X, lengths)
+test_set = [[0], [5], [4], [3], [3]]
 result = model.predict(test_set)
 print(result)
 print("n_features:")
