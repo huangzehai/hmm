463e2c6f52a2590535abf228b93b12458c5b6ef5
huangzehai <chaogeek@foxmail.com>
2019-08-03 19:44:22 +0800

    添加多特性用例

---
+++ hmm/trainingMultiObservationWithMultinomialHMM.py
@@ -0,0 +1,25 @@
+from hmmlearn import hmm
+import numpy as np
+import time
+
+np.random.seed(42)
+
+millis = int(round(time.time()))
+model = hmm.MultinomialHMM(n_components=2, n_iter=1000, tol=0.0001, random_state=millis)
+X1 = [[0], [2], [4], [3], [3], [3]]
+X2 = [[1], [5], [6], [3], [3], [3]]
+X = np.concatenate([X1, X2])
+model.fit(X)
+test_set = [[0], [0], [4], [3], [3], [5]]
+result = model.predict(test_set)
+print(result)
+print "n_features:"
+print model.n_features
+print "startup prob:"
+print model.startprob_
+print "transition prob:"
+print model.transmat_
+print "emission prob:"
+print model.emissionprob_
+print "score:"
+print model.score(test_set)
